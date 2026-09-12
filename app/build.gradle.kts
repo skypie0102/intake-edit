e24fddc712eda718751bd0a9afa3e6fa1f9f49f0
skypie0102 <skypirate1202@gmail.com>
2026-09-13 03:03:17 +0800

    Use real org.json in JVM tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,5 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     debugImplementation("androidx.compose.ui:ui-tooling")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.json:json:20240303")
 }
