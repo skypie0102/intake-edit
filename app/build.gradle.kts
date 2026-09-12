2043463c1b864c4a7198e06646fa0af0b6b73dff
skypie0102 <skypirate1202@gmail.com>
2026-09-12 23:51:50 +0800

    Add rich inline unit test dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,4 +54,5 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     debugImplementation("androidx.compose.ui:ui-tooling")
+    testImplementation("junit:junit:4.13.2")
 }
