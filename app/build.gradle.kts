cfde6d229db73278f91fce6cd3e1edfbd74a4d5c
skypie0102 <skypirate1202@gmail.com>
2026-09-17 21:20:23 +0800

    release: bump to v0.14.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "cloud.shadowmonarchbooks.intakeedit"
         minSdk = 26
         targetSdk = 37
-        versionCode = 33
-        versionName = "0.13.0"
+        versionCode = 34
+        versionName = "0.14.0"
     }
     signingConfigs {
         if (hasReleaseSigning) {
