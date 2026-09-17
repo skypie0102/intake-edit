e0d9b37dc75d2c95598f1ef43d308cc9348171bf
skypie0102 <skypirate1202@gmail.com>
2026-09-18 06:36:49 +0800

    release: bump Intake Edit to v0.15.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "cloud.shadowmonarchbooks.intakeedit"
         minSdk = 26
         targetSdk = 37
-        versionCode = 35
-        versionName = "0.15.0"
+        versionCode = 36
+        versionName = "0.15.1"
     }
     signingConfigs {
         if (hasReleaseSigning) {
